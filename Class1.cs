e42873fc5937b0a0a8620238fe8fda98913e3558
MarcoMedrano <marco.medrano.garay@gmail.com>
2022-08-24 13:51:40 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Markind.Siat
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var s = new ServicioFacturacionCodigosClient();
+            s.cufdAsync()
+
+        }
+    }
+}
