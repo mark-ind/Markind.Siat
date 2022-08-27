f6f4ec37e27b2a12ffbf94452563e1220bf10e00
MarcoMedrano <marco.medrano.garay@gmail.com>
2022-08-27 19:06:37 -0400

    feat: seat class

---
+++ src/CodigoAmbiente.cs
@@ -0,0 +1,7 @@
+namespace Markind.Siat;
+
+public enum CodigoAmbiente
+{
+    Produccion = 1,
+    Pruebas = 2
+}
---
+++ src/MessageBase.cs
@@ -0,0 +1,8 @@
+namespace Markind.Siat;
+
+public class MessageBase
+{
+    public int codigoAmbiente { get; set; }
+    public string codigoSistema { get; set; }
+    public long nit { get; set; }
+}
