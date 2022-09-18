5947e07bc414390b55164b0a8576d3ec5ba9797f
Marco Medrano <marco.medrano.garay@gmail.com>
2022-09-17 21:57:46 -0400

    feat: ToJson ignores null values by default now.

--- src/JsonExtensions.cs
+++ src/JsonExtensions.cs
@@ -14,11 +14,13 @@ public static class ObjectExtensions
     /// </summary>
     /// <param name="value">The value.</param>
     /// <returns>The Json of any object.</returns>
-    public static string ToJson(this object value)
+    public static string ToJson(this object value, bool ignoreNullValues = true)
     {
-        if (value == null) return Null;
+        if(value == null) return Null;
+#pragma warning disable CS0618, SYSLIB0020 // Type or member is obsolete
+        return JsonSerializer.Serialize(value, options: new JsonSerializerOptions() { WriteIndented = true, IgnoreNullValues = ignoreNullValues });
+#pragma warning restore CS0618, SYSLIB0020 // Type or member is obsolete
 
-        return JsonSerializer.Serialize(value, options: new JsonSerializerOptions(){WriteIndented = true});
     }
 
     public static void Dump(this object value)
