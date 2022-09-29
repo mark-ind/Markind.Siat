12f5a1b21cb9a810b6dd80e793323a83d9def0ec
Marco Medrano <marco.medrano.garay@gmail.com>
2022-09-29 16:58:32 -0400

    feat: generated facturas

--- generateFacs.ps1
+++ generateFacs.ps1
@@ -1,8 +1,9 @@
-$files = Get-ChildItem res/
-foreach ($xsdFileName in $files)
+$files = Get-ChildItem res/facturas
+
+$args = @()
+foreach ($file in $files)
 {
-    $name = $xsdFileName.ToString().Replace("factura", "").Replace(".xsd", "");
-    echo $xsdFileName.ToString()
-    echo $name
-    xscgen -o src/Facturas --nc --nu -n "|$($xsdFileName)=Markind.Siat.Generated.Facturas.$($name)" res/$($xsdFileName)
+    $args += "res/facturas/$($file.Name)";
 }
+
+& xscgen -o src/Facturas --nc --nu --cn -n =Markind.Siat.Generated $args
