d33967d7af7192fbc98129ee7a2b732ac15d827c
MarcoMedrano <marco.medrano.garay@gmail.com>
2023-01-25 12:23:48 -0400

    feat: added default date

--- src/SolicitudRecepcionFacturaComputarizada.cs
+++ src/SolicitudRecepcionFacturaComputarizada.cs
@@ -15,7 +15,7 @@ public class SolicitudRecepcionFacturaComputarizada : SolicitudRecepcion
     }
 
     public required byte[] Archivo { get; set; }
-    public required string FechaEnvio { get; set; }
+    public required string FechaEnvio { get; set; } = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fff");
     public required string HashArchivo { get; set; }
 
     public static implicit operator Generated.ServicioFacturacionCompraVenta.solicitudRecepcionFactura(SolicitudRecepcionFacturaComputarizada src) => Cast<SolicitudRecepcionFacturaComputarizada, Generated.ServicioFacturacionCompraVenta.solicitudRecepcionFactura>(src);
